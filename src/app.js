6aaeae9fc3e9685b3ec7208e07a9cce34ae7bc9f
Suhaib Ersan <suhaib.m.ersan@gmail.com>
2021-10-06 23:57:25 +0300

    90% done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import ToDo from './components/todo/todo.js';
+import ToDo from "./components/todo/todo.js";
+import TodoItemsProvider from "./context/todoItems.context.js";
+import SettingsProvider from "./context/settings.context.js";
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <ToDo />
-    );
-  }
+    render() {
+        return (
+            <SettingsProvider>
+                <TodoItemsProvider>
+                    <ToDo />
+                </TodoItemsProvider>
+            </SettingsProvider>
+        );
+    }
 }
